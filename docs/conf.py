33ed6b9ce350af70b1d7d95c27f1da601bc04039
Tom Cobb <tom.cobb@diamond.ac.uk>
2019-01-11 15:55:37 +0000

    If it exists, require sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -120,6 +120,8 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
     try:
+        from pkg_resources import require
+        require("sphinx_rtd_theme")
         import sphinx_rtd_theme
         html_theme = 'sphinx_rtd_theme'
         html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
