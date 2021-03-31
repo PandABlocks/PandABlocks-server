4053572074559db972ec0559304c2b409245def4
Michael Abbott <michael.abbott@diamond.ac.uk>
2021-03-31 11:36:17 +0100

    Add support for multi-register read/write to extension server
    This is in preparation for github issue #9

--- python/test_extension/dummy.py
+++ python/test_extension/dummy.py
@@ -14,8 +14,22 @@ class DummyField:
     def write(self, number, value):
         self.values[number] = value
 
+class PolyField:
+    def __init__(self, parent):
+        logging.info('poly field')
+
+    def read(self, number, *values):
+        return sum(values)
+
+    def write(self, number, *values):
+        return reversed(values)
+
 
 class Extension:
+    constructors = {
+        'poly' : PolyField,
+    }
+
     def __init__(self, count):
         logging.info('dummy %d', count)
         self.count = count
@@ -24,7 +38,8 @@ class Extension:
     def make_field(self, name):
         logging.info('make_field %s', name)
         if name not in self.fields:
-            self.fields[name] = DummyField(self)
+            constructor = self.constructors.get(name, DummyField)
+            self.fields[name] = constructor(self)
         return self.fields[name]
 
     def parse_read(self, node):
