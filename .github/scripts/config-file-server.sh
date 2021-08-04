197e69f85b9d08d93eb136e431c29714bb9525ef
Michael Stubbings <michael.stubbings@diamond.ac.uk>
2021-08-04 10:49:36 +0100

    Add brackets to environment variables

--- .github/scripts/config-file-server.sh
+++ .github/scripts/config-file-server.sh
@@ -12,7 +12,7 @@ if [ "$PLATFORM" == "zynq" ];
 then
 cat >> CONFIG <<EOL
 # Default build location. Default is to build in build subdirectory.
-BUILD_DIR = \$GITHUB_WORKSPACE/build-server
+BUILD_DIR = \$(GITHUB_WORKSPACE)/build-server
 
 # Python interpreter. Default interpreter is python3
 PYTHON = python3
@@ -24,16 +24,16 @@ SPHINX_BUILD = sphinx-build
 # is specified it will be prepended to the path for building the driver and
 # target server.
 #
-TOOLCHAIN_ROOT = \$GITHUB_WORKSPACE/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf
+TOOLCHAIN_ROOT = \$(GITHUB_WORKSPACE)/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf
 
 # Where the kernel was compiled.  Use PandABlocks-rootfs to build this.  This is
 # only required if building the driver target.
 #
-KERNEL_DIR = \$GITHUB_WORKSPACE/build/build/linux
+KERNEL_DIR = \$(GITHUB_WORKSPACE)/build/build/linux
 
 # Some tools from the PandABlocks-rootfs are used.
 #
-PANDA_ROOTFS = \$GITHUB_WORKSPACE/PandABlocks-rootfs
+PANDA_ROOTFS = \$(GITHUB_WORKSPACE)/PandABlocks-rootfs
 
 # List of default targets build when running make
 #
@@ -47,7 +47,7 @@ elif [ "$PLATFORM" == "zynqmp" ]
 then
 cat >> CONFIG <<EOL
 # Default build location.  Default is to build in build subdirectory.
-BUILD_DIR = \$GITHUB_WORKSPACE/build-server
+BUILD_DIR = \$(GITHUB_WORKSPACE)/build-server
 
 # Python interpreter.  Default interpreter is python3
 PYTHON = python3
@@ -59,16 +59,16 @@ SPHINX_BUILD = sphinx-build
 # is specified it will be prepended to the path for building the driver and
 # target server.
 #
-TOOLCHAIN_ROOT = \$GITHUB_WORKSPACE/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu
+TOOLCHAIN_ROOT = \$(GITHUB_WORKSPACE)/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu
 
 # Where the kernel was compiled.  Use PandABlocks-rootfs to build this.  This is
 # only required if building the driver target.
 #
-KERNEL_DIR = \$GITHUB_WORKSPACE/build/build/linux
+KERNEL_DIR = \$(GITHUB_WORKSPACE)/build/build/linux
 
 # Some tools from the PandABlocks-rootfs are used.
 #
-PANDA_ROOTFS = \$GITHUB_WORKSPACE/PandABlocks-rootfs
+PANDA_ROOTFS = \$(GITHUB_WORKSPACE)/PandABlocks-rootfs
 
 # List of default targets build when running make
 #
