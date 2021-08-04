dc18601cecabbfe9d99e313611fa77a7e6e52cf6
Michael Stubbings <michael.stubbings@diamond.ac.uk>
2021-08-04 10:32:45 +0100

    replace single quotations for double quotations

--- .github/scripts/config-files.sh
+++ .github/scripts/config-files.sh
@@ -12,7 +12,7 @@ if [ "$PLATFORM" == "zynq" ];
 then
 cat >> CONFIG <<EOL
 # Default build location. Default is to build in build subdirectory.
-BUILD_DIR = '$GITHUB_WORKSPACE'/build-server
+BUILD_DIR = "$GITHUB_WORKSPACE"/build-server
 
 # Python interpreter. Default interpreter is python3
 PYTHON = python3
@@ -25,16 +25,16 @@ SPHINX_BUILD = sphinx-build
 # target server.
 #
 TOOLCHAIN_ROOT = \
-     '$GITHUB_WORKSPACE'/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf
+     "$GITHUB_WORKSPACE"/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf
 
 # Where the kernel was compiled.  Use PandABlocks-rootfs to build this.  This is
 # only required if building the driver target.
 #
-KERNEL_DIR = '$GITHUB_WORKSPACE'/build/build/linux
+KERNEL_DIR = "$GITHUB_WORKSPACE"/build/build/linux
 
 # Some tools from the PandABlocks-rootfs are used.
 #
-PANDA_ROOTFS = '$GITHUB_WORKSPACE'/PandABlocks-rootfs
+PANDA_ROOTFS = "$GITHUB_WORKSPACE"/PandABlocks-rootfs
 
 # List of default targets build when running make
 #
@@ -48,7 +48,7 @@ elif [ "$PLATFORM" == "zynqmp" ]
 then
 cat >> CONFIG <<EOL
 # Default build location.  Default is to build in build subdirectory.
-BUILD_DIR = '$GITHUB_WORKSPACE'/build-server
+BUILD_DIR = "$GITHUB_WORKSPACE"/build-server
 
 # Python interpreter.  Default interpreter is python3
 PYTHON = python3
@@ -61,16 +61,16 @@ SPHINX_BUILD = sphinx-build
 # target server.
 #
 TOOLCHAIN_ROOT = \
-     '$GITHUB_WORKSPACE'/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu
+     "$GITHUB_WORKSPACE"/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu
 
 # Where the kernel was compiled.  Use PandABlocks-rootfs to build this.  This is
 # only required if building the driver target.
 #
-KERNEL_DIR = '$GITHUB_WORKSPACE'/build/build/linux
+KERNEL_DIR = "$GITHUB_WORKSPACE"/build/build/linux
 
 # Some tools from the PandABlocks-rootfs are used.
 #
-PANDA_ROOTFS = '$GITHUB_WORKSPACE'/PandABlocks-rootfs
+PANDA_ROOTFS = "$GITHUB_WORKSPACE"/PandABlocks-rootfs
 
 # List of default targets build when running make
 #
@@ -91,16 +91,16 @@ if [ "$PLATFORM" == "zynq" ];
 then
 cat >> CONFIG <<EOL
 # Location of rootfs builder
-ROOTFS_TOP = $GITHUB_WORKSPACE/rootfs
+ROOTFS_TOP = "$GITHUB_WORKSPACE"/rootfs
 
 # Toolchain used to build the target
-TOOLCHAIN_ROOT = '$GITHUB_WORKSPACE'/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf
+TOOLCHAIN_ROOT = "$GITHUB_WORKSPACE"/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf
 
 # Where to find source files
-TAR_FILES = '$GITHUB_WORKSPACE'/tar-files
+TAR_FILES = "$GITHUB_WORKSPACE"/tar-files
 
 # Target location for build
-PANDA_ROOT = '$GITHUB_WORKSPACE'/build
+PANDA_ROOT = "$GITHUB_WORKSPACE"/build
 
 # Whether the platform is zynq or zyqnmp
 PLATFORM = zynq
@@ -109,16 +109,16 @@ elif [ "$PLATFORM" == "zynqmp" ]
 then
 cat >> CONFIG <<EOL
 # Location of rootfs builder
-ROOTFS_TOP = '$GITHUB_WORKSPACE'/rootfs
+ROOTFS_TOP = "$GITHUB_WORKSPACE"/rootfs
 
 # Toolchain used to build the target
-TOOLCHAIN_ROOT = '$GITHUB_WORKSPACE'/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu
+TOOLCHAIN_ROOT = "$GITHUB_WORKSPACE"/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu
 
 # Where to find source files
-TAR_FILES = '$GITHUB_WORKSPACE'/tar-files
+TAR_FILES = "$GITHUB_WORKSPACE"/tar-files
 
 # Target location for build
-PANDA_ROOT = '$GITHUB_WORKSPACE'/build
+PANDA_ROOT = "$GITHUB_WORKSPACE"/build
 
 # Whether the platform is zynq or zyqnmp
 PLATFORM = zynqmp
@@ -134,8 +134,8 @@ cat >> CONFIG.local <<EOL
 TARGET = minimal
 
 # This is the location where source and build files will be placed.
-server_ROOT = '$GITHUB_WORKSPACE'/build
+server_ROOT = "$GITHUB_WORKSPACE"/build
 
 # This is where all of the source tar files will be found.
-TAR_DIRS = '$GITHUB_WORKSPACE'/tar-files
+TAR_DIRS = "$GITHUB_WORKSPACE"/tar-files
 EOL
