236a7fd3d5e8bf6523e73c9391f89e9493b6d84a
Michael Stubbings <michael.stubbings@diamond.ac.uk>
2021-08-05 09:42:10 +0100

    Remove shell parameter from run actions
    Remove duplicate cat commands in config-file-server.sh

--- .github/scripts/config-file-server.sh
+++ .github/scripts/config-file-server.sh
@@ -7,9 +7,17 @@ PLATFORM=$1
 # Create the CONFIG file
 cd PandABlocks-server
 touch CONFIG
-# Populate the CONFIG file
+
+#Determine the toolchain to use
 if [ "$PLATFORM" == "zynq" ]; 
 then
+TOOLCHAIN=gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf
+elif [ "$PLATFORM" == "zynqmp" ]
+then
+TOOLCHAIN=gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu
+fi
+
+# Populate the CONFIG file
 cat >> CONFIG <<EOL
 # Default build location. Default is to build in build subdirectory.
 BUILD_DIR = \$(GITHUB_WORKSPACE)/build-server
@@ -24,7 +32,7 @@ SPHINX_BUILD = sphinx-build
 # is specified it will be prepended to the path for building the driver and
 # target server.
 #
-TOOLCHAIN_ROOT = \$(GITHUB_WORKSPACE)/gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf
+TOOLCHAIN_ROOT = \$(GITHUB_WORKSPACE)/$TOOLCHAIN
 
 # Where the kernel was compiled.  Use PandABlocks-rootfs to build this.  This is
 # only required if building the driver target.
@@ -41,41 +49,5 @@ DEFAULT_TARGETS =  server sim_server docs driver
 #
 # Whether it will run in platform zynq or zynqmp
 #
-PLATFORM = zynq
+PLATFORM = $PLATFORM
 EOL
-elif [ "$PLATFORM" == "zynqmp" ]
-then
-cat >> CONFIG <<EOL
-# Default build location.  Default is to build in build subdirectory.
-BUILD_DIR = \$(GITHUB_WORKSPACE)/build-server
-
-# Python interpreter.  Default interpreter is python3
-PYTHON = python3
-
-# Sphinx build for documentation.
-SPHINX_BUILD = sphinx-build
-
-# Compiler settings.  Default cross compile prefix given here.  If BINUTILS_DIR
-# is specified it will be prepended to the path for building the driver and
-# target server.
-#
-TOOLCHAIN_ROOT = \$(GITHUB_WORKSPACE)/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu
-
-# Where the kernel was compiled.  Use PandABlocks-rootfs to build this.  This is
-# only required if building the driver target.
-#
-KERNEL_DIR = \$(GITHUB_WORKSPACE)/build/build/linux
-
-# Some tools from the PandABlocks-rootfs are used.
-#
-PANDA_ROOTFS = \$(GITHUB_WORKSPACE)/PandABlocks-rootfs
-
-# List of default targets build when running make
-#
-DEFAULT_TARGETS =  server sim_server docs driver
-#
-# Whether it will run in platform zynq or zynqmp
-#
-PLATFORM = zynqmp
-EOL
-fi
