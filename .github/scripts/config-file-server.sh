b483468b463804232db760666bd0017893fd34b0
Michael Stubbings <michael.stubbings@diamond.ac.uk>
2021-08-23 13:39:28 +0100

    Add release job to make-server workflow
    Update sytax of CONFIG generation script

--- .github/scripts/config-file-server.sh
+++ .github/scripts/config-file-server.sh
@@ -11,9 +11,9 @@ elif [ "$PLATFORM" == "zynqmp" ]; then
 fi
 
 # PandABlocks-server:
-cat >> PandABlocks-server/CONFIG << EOL
+cat >> PandABlocks-server/CONFIG << 'EOL'
 # Default build location. Default is to build in build subdirectory.
-BUILD_DIR = \$(GITHUB_WORKSPACE)/build-server
+BUILD_DIR = $(GITHUB_WORKSPACE)/build-server
 
 # Python interpreter. Default interpreter is python3
 PYTHON = python3
@@ -21,21 +21,25 @@ PYTHON = python3
 # Sphinx build for documentation.
 SPHINX_BUILD = sphinx-build
 
+# Where the kernel was compiled.  Use PandABlocks-rootfs to build this.  This is
+# only required if building the driver target.
+KERNEL_DIR = $(GITHUB_WORKSPACE)/build/build/linux
+
+# Some tools from the PandABlocks-rootfs are used.
+PANDA_ROOTFS = $(GITHUB_WORKSPACE)/PandABlocks-rootfs
+
+# List of default targets build when running make
+DEFAULT_TARGETS = server sim_server docs driver
+
+EOL
+
+cat >> PandABlocks-server/CONFIG << EOL
 # Compiler settings. Default cross compile prefix given here.  If BINUTILS_DIR
 # is specified it will be prepended to the path for building the driver and
 # target server.
 TOOLCHAIN_ROOT = \$(GITHUB_WORKSPACE)/$TOOLCHAIN
 
-# Where the kernel was compiled.  Use PandABlocks-rootfs to build this.  This is
-# only required if building the driver target.
-KERNEL_DIR = \$(GITHUB_WORKSPACE)/build/build/linux
-
-# Some tools from the PandABlocks-rootfs are used.
-PANDA_ROOTFS = \$(GITHUB_WORKSPACE)/PandABlocks-rootfs
-
-# List of default targets build when running make
-DEFAULT_TARGETS = server sim_server docs driver
-
 # Whether it will run in platform zynq or zynqmp
 PLATFORM = $PLATFORM
+
 EOL
