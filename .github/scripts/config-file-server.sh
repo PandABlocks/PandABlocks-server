76b79501e158befe56463db0960a6c2bfe7a70ae
Michael Stubbings <michael.stubbings@diamond.ac.uk>
2021-09-13 10:26:21 +0100

    Update error condition of platform

--- .github/scripts/config-file-server.sh
+++ .github/scripts/config-file-server.sh
@@ -8,7 +8,8 @@ if [ "$PLATFORM" == "zynq" ]; then
     TOOLCHAIN=gcc-arm-10.2-2020.11-x86_64-arm-none-linux-gnueabihf
 elif [ "$PLATFORM" == "zynqmp" ]; then
     TOOLCHAIN=gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu
-else >&2 echo "Argument $PLATFORM must be either zynq or zynqmp"
+else >&2 echo "Error: Argument $PLATFORM must be either zynq or zynqmp"
+    exit 0
 fi
 
 # Populate CONFIG
