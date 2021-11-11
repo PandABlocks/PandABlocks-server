ed41f20b6c4a04c1c3808f73cc667e841866fd6e
Araneidae <Araneidae@users.noreply.github.com>
2021-11-11 08:16:36 +0000

    deploy: d9e0ca915ed9698f08504646c76ab8ec091ffaef

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.2a1-23-g6c6be0b',
+    VERSION: '2.2a1-25-gd9e0ca9',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
