a5baed6cc2df0422c89d02d751a1c8c774398ac2
EmilioPeJu <EmilioPeJu@users.noreply.github.com>
2024-01-15 17:22:52 +0000

    deploy: e5a5dab5512a008d6310e63bc2dbaad4a845c736

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0b1-10-g54227ee',
+    VERSION: '3.0-1-ge5a5dab',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
