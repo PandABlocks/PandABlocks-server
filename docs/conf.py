b1c15f5f6beedcef6b9f0c65097eebdc927db616
Shihab Suliman <rye74444@ws557.diamond.ac.uk>
2024-05-23 11:48:20 +0100

    commands file

--- docs/conf.py
+++ docs/conf.py
@@ -183,5 +183,5 @@ html_show_sphinx = False
 html_show_copyright = False
 
 # Logo
-html_logo = "images/dls-logo.svg"
-html_favicon = html_logo
+html_logo = "images/PandA-logo-for-black-background.svg"
+html_favicon = "images/favicon.ico"
